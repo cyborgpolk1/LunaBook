fffb401ec74f3c5038bc642564a10fec3b82a2b7
cyborgpolk1 <hliatis1@gmail.com>
2019-10-21 00:42:04 -0400

    Add MipMap Crate
    Adds Basic Texture and Lighting shader too

---
+++ Shaders/BasicTextureAndLighting.hlsl
@@ -0,0 +1,92 @@
+#include "LightHelper.hlsl"
+
+cbuffer cbPerFrame : register(b0)
+{
+    DirectionalLight gDirLight;
+    PointLight gPointLight;
+    SpotLight gSpotLight;
+    float3 gEyePosW;
+    float pad;
+};
+
+cbuffer cbPerObject : register(b1)
+{
+    float4x4 gWorld;
+    float4x4 gWorldInvTranspose;
+    float4x4 gWorldViewProj;
+    Material gMaterial;
+};
+
+struct VertexIn
+{
+    float3 PosL : POSITION;
+    float3 NormalL : NORMAL;
+    float2 TexC : TEXCOORD;
+};
+
+struct VertexOut
+{
+    float4 PosH : SV_POSITION;
+    float3 PosW : POSITION;
+    float3 NormalW : NORMAL;
+    float2 TexC : TEXCOORD;
+};
+
+Texture2D gTex;
+SamplerState gSample;
+
+VertexOut VS(VertexIn vin)
+{
+    VertexOut vout;
+
+    // Transform to world space.
+    vout.PosW = mul(float4(vin.PosL, 1.0f), gWorld).xyz;
+    vout.NormalW = mul(vin.NormalL, (float3x3) gWorldInvTranspose);
+
+    // Transform to homogenous clip space.
+    vout.PosH = mul(float4(vin.PosL, 1.0f), gWorldViewProj);
+
+    vout.TexC = vin.TexC;
+
+    return vout;
+}
+
+float4 PS(VertexOut pin) : SV_Target
+{
+    // Interpolating normal can unnormalize it, so normalize it.
+    pin.NormalW = normalize(pin.NormalW);
+
+    float3 toEyeW = normalize(gEyePosW - pin.PosW);
+
+    float4 texColor = gTex.Sample(gSample, pin.TexC);
+
+    // Start with a sum of zero.
+    float4 ambient = float4(0.0f, 0.0f, 0.0f, 0.0f);
+    float4 diffuse = float4(0.0f, 0.0f, 0.0f, 0.0f);
+    float4 spec = float4(0.0f, 0.0f, 0.0f, 0.0f);
+
+    // sum the light contribution from each light source.
+    float4 A, D, S;
+
+    ComputeDirectionalLight(gMaterial, gDirLight, pin.NormalW, toEyeW, A, D, S);
+    ambient += A;
+    diffuse += D;
+    spec += S;
+
+    ComputePointLight(gMaterial, gPointLight, pin.PosW, pin.NormalW, toEyeW, A, D, S);
+    ambient += A;
+    diffuse += D;
+    spec += S;
+
+    ComputeSpotLight(gMaterial, gSpotLight, pin.PosW, pin.NormalW, toEyeW, A, D, S);
+    ambient += A;
+    diffuse += D;
+    spec += S;
+
+    float4 litColor = texColor * (ambient + diffuse) + spec;
+
+    // Common to take alpha from diffuse material.
+    litColor.a = gMaterial.Diffuse.a * texColor.a;
+
+    return litColor;
+}
---
+++ Source/MipmapCrate/MipmapCrate.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "D3DApp.h"
+#include "MathHelper.h"
+#include "GeometryGenerator.h"
+#include "DDSTextureLoader.h"
+
+struct Vertex
+{
+	XMFLOAT3 Pos;
+	XMFLOAT2 Tex;
+};
+
+struct MatrixBuffer
+{
+	XMMATRIX WorldViewProj;
+};
+
+class CrateApp : public D3DApp
+{
+public:
+	CrateApp(HINSTANCE hInstance);
+	~CrateApp();
+
+	bool Init();
+	void OnResize();
+	void UpdateScene(float dt);
+	void DrawScene();
+
+	void OnMouseDown(WPARAM btnState, int x, int y);
+	void OnMouseUp(WPARAM btnState, int x, int y);
+	void OnMouseMove(WPARAM btnState, int x, int y);
+
+private:
+	void BuildGeometryBuffers();
+	void BuildFX();
+	void BuildTex();
+
+private:
+	ID3D11Buffer* mVB;
+	ID3D11Buffer* mIB;
+	ID3D11VertexShader* mVS;
+	ID3D11PixelShader* mPS;
+	ID3D11Buffer* mMatrixBuffer;
+
+	UINT mIndexCount;
+
+	ID3D11InputLayout* mInputLayout;
+
+	ID3D11ShaderResourceView* mTexture;
+	ID3D11SamplerState* mSampleState;
+
+	XMFLOAT4X4 mWorld;
+	XMFLOAT4X4 mView;
+	XMFLOAT4X4 mProj;
+
+	float mTheta;
+	float mPhi;
+	float mRadius;
+
+	POINT mLastMousePoint;
+};
