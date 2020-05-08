0babfc11ed9cbc823b6dab458a6f590fd5e34d14
cyborgpolk1 <hliatis1@gmail.com>
2020-05-07 21:09:24 -0700

    Update Ex19.cpp to show original vectors

--- Source/EX19/Ex19.cpp
+++ Source/EX19/Ex19.cpp
@@ -35,22 +35,30 @@ int main()
 	XMVECTOR v = XMVectorSet(-2.0f, 1.0f, -3.0f, 2.5f);
 	XMVECTOR w = XMVectorSet(0.0f, XM_PIDIV4, XM_PIDIV2, XM_PI);
 
+	cout << "p					= " << p << endl;
+	cout << "q					= " << q << endl;
+	cout << "u					= " << u << endl;
+	cout << "v					= " << v << endl;
+	cout << "w					= " << w << endl;
+
+	cout << endl;
+
 	cout << "XMvectorAbs(v)				= " << XMVectorAbs(v) << endl;
 	cout << "XMVectorCos(w)				= " << XMVectorCos(w) << endl;
 	cout << "XMVectorLog(u)				= " << XMVectorLog(u) << endl;
 	cout << "XMVectorExp(p)				= " << XMVectorExp(p) << endl;
 
 	cout << "XMVectorPow(u, p)			= " << XMVectorPow(u, p) << endl;
-	cout << "XMVectorSqrt(u)			= " << XMVectorSqrt(u) << endl;
+	cout << "XMVectorSqrt(u)				= " << XMVectorSqrt(u) << endl;
 
-	cout << "XMVectorSwizzle(u, 2, 2, 1, 3) = "
+	cout << "XMVectorSwizzle(u, 2, 2, 1, 3)		= "
 		<< XMVectorSwizzle(u, 2, 2, 1, 3) << endl;
-	cout << "XMVectorSwizzle(u, 2, 1, 0, 3) = "
+	cout << "XMVectorSwizzle(u, 2, 1, 0, 3)		= "
 		<< XMVectorSwizzle(u, 2, 1, 0, 3) << endl;
 
-	cout << "XMVectorMultiply(u, v)		= " 
+	cout << "XMVectorMultiply(u, v)			= " 
 		<< XMVectorMultiply(u, v) << endl;
-	cout << "XMVectorSaturate(q)		= "
+	cout << "XMVectorSaturate(q)			= "
 		<< XMVectorSaturate(q) << endl;
 	cout << "XMVectorMin(p, v)			= " << XMVectorMin(p, v) << endl;
 	cout << "XMVectorMax(p, v)			= " << XMVectorMax(p, v) << endl;
