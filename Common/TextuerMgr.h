28dc9e0fb271fad145fe98ea133cb7471c88e913
cyborgpolk1 <hliatis1@gmail.com>
2021-06-01 02:20:09 -0400

    Add Texture Manager

---
+++ Common/TextuerMgr.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "d3dUtil.h"
+#include <map>
+
+///<summary>
+/// Simple texture manager to avoid laoding duplicate textures from file. That can
+/// happen, for example, if multiple meshes reference the same texture filename.
+///</summary>
+class TextureMgr
+{
+public:
+    TextureMgr();
+    ~TextureMgr();
+
+    void Init(ID3D11Device* device);
+
+    ID3D11ShaderResourceView* CreateTexture(std::wstring filename);
+
+private:
+    TextureMgr(const TextureMgr& rhs);
+    TextureMgr& operator=(const TextureMgr& rhs);
+
+private:
+    ID3D11Device* md3dDevice;
+    std::map<std::wstring, ID3D11ShaderResourceView*> mTextureSRV;
+};
